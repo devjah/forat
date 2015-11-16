32a421e211218ed5fe8a5add00d2fe5f4c232afd
Eugeniu Vozoca <devjahkey@gmail.com>
2015-11-16 15:09:38 +0200

    request fb link

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,5 +252,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   require "omniauth-facebook"
-  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET'], scope: 'public_profile', info_fields: 'name, email, picture'
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET'], scope: 'public_profile', info_fields: 'name, email, picture, link'
 end
